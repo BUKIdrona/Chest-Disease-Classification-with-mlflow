2546fed32d258c82c2ebf82afb5aac9136b9ab42
BUKIdrona <deepak.mandal.ug21@nsut.ac.in>
2024-05-02 22:46:08 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,61 @@
-# from src.ChestCancerClassifer import logger
 from ChestCancerClassifer import logger
+from ChestCancerClassifer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from ChestCancerClassifer.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+# from ChestCancerClassifer.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+# from ChestCancerClassifer.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
-logger.info("Welcome to ChestClassifier!")
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+# STAGE_NAME = "Prepare base model"
+# try: 
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    prepare_base_model = PrepareBaseModelTrainingPipeline()
+#    prepare_base_model.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+
+
+
+# STAGE_NAME = "Training"
+# try: 
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    model_trainer = ModelTrainingPipeline()
+#    model_trainer.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+
+
+
+
+# STAGE_NAME = "Evaluation stage"
+# try:
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#    model_evalution = EvaluationPipeline()
+#    model_evalution.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+# except Exception as e:
+#         logger.exception(e)
+#         raiseChestCancer
--- src/ChestCancerClassifer/utils/common.py
+++ src/ChestCancerClassifer/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from cnnClassifier import logger
+from ChestCancerClassifer import logger
 import json
 import joblib
 from ensure import ensure_annotations
