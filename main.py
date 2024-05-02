d6e130e97753947215e0039c9fe6acd743dac921
BUKIdrona <deepak.mandal.ug21@nsut.ac.in>
2024-05-03 01:01:08 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ChestCancerClassifer import logger
 from ChestCancerClassifer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from ChestCancerClassifer.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from ChestCancerClassifer.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 # from ChestCancerClassifer.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 # from ChestCancerClassifer.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
@@ -19,16 +19,16 @@ except Exception as e:
     raise e
 
 
-# STAGE_NAME = "Prepare base model"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    prepare_base_model = PrepareBaseModelTrainingPipeline()
-#    prepare_base_model.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
